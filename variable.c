372ec22463a53cd8ad92d52634158aa8089a3ac5
maddanasaileela-prog <maddanasaileela@gmail.com>
2025-12-09 23:01:27 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int Firstnumber = 33;
+    printf("Firstnumber = %d\n", Firstnumber);
+
+    int Secondnumber = Firstnumber;
+    printf("Secondnumber = %d\n", Secondnumber);
+
+    return 0;
+}
